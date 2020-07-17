84fb08da0f83f99c372c6b766da31c96708e7738
Reshetnikov Alexandr <hemn.still@gmail.com>
2020-07-17 13:58:21 +0400

    Добавил возможность запускать из сетевой папки + в папке с пробелами.

--- X-MouseButtonControl/create_install.bat
+++ X-MouseButtonControl/create_install.bat
@@ -1,13 +1,12 @@
 @echo off
-cd /d "%~dp0"
-set latest_filename=XMouseButtonControlSetup.exe
+set latest_filename=X-MouseButtonControlSetup.exe
 set downloadurl=https://www.highrez.co.uk/scripts/download.asp?package=XMouse
 echo Downloading: %latest_filename% %downloadurl% ...
-..\curl --fail --location %downloadurl% --output %latest_filename%
+"%~dp0..\curl" --fail --location %downloadurl% --output "%~dp0%latest_filename%"
 IF %ERRORLEVEL% NEQ 0 ( 
 	exit
 ) 
 echo Done.
 
 echo Generating %latest_filename% autoinstall.bat
-echo %%~dp0%latest_filename% /S > autoinstall.bat
+echo "%%~dp0%latest_filename%" /S > "%~dp0autoinstall.bat"
