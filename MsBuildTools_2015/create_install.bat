e07bc96a8d2bffe090069827582e52fbc950fdc7
Reshetnikov Alexandr <hemn.still@gmail.com>
2020-07-22 18:43:39 +0400

    Добавил "Microsoft Build Tools 2015 Update 3" https://github.com/microsoft/msbuild/issues/480

---
+++ MsBuildTools_2015/create_install.bat
@@ -0,0 +1,13 @@
+@echo off
+pushd "%~dp0"
+set latest_filename=BuildTools_Full.exe
+set downloadurl=https://go.microsoft.com/fwlink/?LinkId=615458
+echo Downloading: %latest_filename% %downloadurl% ...
+..\curl --fail --location %downloadurl% --output %latest_filename%
+IF %ERRORLEVEL% NEQ 0 ( 
+	exit
+) 
+echo Done.
+
+echo Generating %latest_filename% autoinstall.bat
+echo "%%~dp0%latest_filename%" /passive > autoinstall.bat
