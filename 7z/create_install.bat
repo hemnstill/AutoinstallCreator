3122848b12783cc8967e1b70d2da4fd9819613c3
Reshetnikov Alexandr <hemn.still@gmail.com>
2020-07-23 23:19:36 +0400

    Установка 7-zip

---
+++ 7z/create_install.bat
@@ -0,0 +1,26 @@
+@echo off
+pushd "%~dp0"
+
+set latest_version=https://www.7-zip.org/download.html
+>raw_download_str.tmp (
+  ..\curl -s %latest_version% | ..\grep --only-matching "[^ ]*x64.msi"
+)
+IF %ERRORLEVEL% NEQ 0 ( 
+  echo Cannot get latest version 
+  exit
+) 
+
+set /p downloadurl=< raw_download_str.tmp
+set downloadurl=https://www.7-zip.org/%downloadurl:~6%
+echo Downloading: %downloadurl% ...
+..\curl --fail --remote-name --location %downloadurl%
+IF %ERRORLEVEL% NEQ 0 ( 
+	exit
+) 
+echo Done.
+
+FOR %%i IN ("%downloadurl%") DO (
+	set latest_filename=%%~ni%%~xi
+)
+echo Generating %latest_filename% autoinstall.bat
+echo "%%~dp0%latest_filename%" /passive > autoinstall.bat
