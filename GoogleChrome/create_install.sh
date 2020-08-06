b5800cc4511f9109371bd7b2a944561c81b1303b
Reshetnikov Alexandr <hemn.still@gmail.com>
2020-08-07 00:20:23 +0400

    GoogleChrome для Ubuntu.

---
+++ GoogleChrome/create_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd "$(dirname "${BASH_SOURCE[0]}")"
+function curl() { if [[ $(uname) == CYGWIN* ]]; then ../curl.exe $@; else ../curl --cacert ../curl-ca-bundle.crt $@; fi }
+
+download_url='https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'
+curl --fail -L $download_url --remote-name
+{
+  echo '#!/bin/bash'
+  echo 'cd "$(dirname "${BASH_SOURCE[0]}")"'
+  echo dpkg -i $(basename -- "$download_url")
+} > autoinstall.sh
+chmod +x ./autoinstall.sh
+
+echo Done.
