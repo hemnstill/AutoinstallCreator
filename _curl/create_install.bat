9fcf0721101026cf0775f126ef88c969d7f41a8b
Reshetnikov Alexandr <hemn.still@gmail.com>
2020-08-18 23:35:29 +0400

    Автообновление curl

--- _curl/create_install.bat
+++ _curl/create_install.bat
@@ -1,9 +1,12 @@
 @echo off
 pushd "%~dp0"
+set curl=..\curl --fail
+set grep=..\grep
+set p7z=..\7za
 
 set latest_version=https://curl.haxx.se/windows/
 >raw_download_str.tmp (
-  ..\curl -s %latest_version% | ..\grep --only-matching "dl[^ ]*win64-mingw.zip"
+  %curl% -s %latest_version% | %grep% --only-matching "dl[^ ]*win64-mingw.zip"
 )
 IF %ERRORLEVEL% NEQ 0 ( 
   echo Cannot get latest version 
@@ -13,8 +16,18 @@ IF %ERRORLEVEL% NEQ 0 (
 set /p downloadurl=< raw_download_str.tmp
 set downloadurl=%latest_version%%downloadurl%
 echo Downloading: %downloadurl% ...
-..\curl --fail --remote-name --location %downloadurl%
+%curl% --remote-name --location %downloadurl%
 IF %ERRORLEVEL% NEQ 0 ( 
-	exit
+  exit
 ) 
+echo Done.
+
+FOR %%i IN ("%downloadurl%") DO (
+  set latest_filename=%%~ni%%~xi
+)
+
+echo Generating %latest_filename% autoinstall.bat
+>autoinstall.bat (
+  echo "%%~dp0%p7z%" e "%latest_filename%" "-o%%~dp0.." *.exe *.crt -aoa -r
+)
 echo Done.
