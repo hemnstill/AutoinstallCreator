81cdd65b5fd609a64686e00b0c7a1b536a30247e
Reshetnikov Alexandr <hemn.still@gmail.com>
2020-09-04 00:16:49 +0400

    Автоустановка nvidia drivers

---
+++ Nvidia/create_install.bat
@@ -0,0 +1,78 @@
+@echo off
+pushd "%~dp0"
+set curl=..\curl
+set LC_ALL=en_US.UTF-8
+set grep=..\grep
+
+set lang=en-us
+rem dhc mode. 
+set dtc_id=0
+set product_series_typeid=2
+set os_typeid=4
+set lookup_base_url=https://www.nvidia.com/Download/API/lookupValueSearch.aspx?TypeID=
+set find_base_url=https://www.nvidia.com/Download/processDriver.aspx?
+set download_base_url=https://us.download.nvidia.com
+
+set os_name=Windows 10 64-bit
+set product_series=%~1
+if "%~1"=="" (
+  echo product_series does not set:
+  >raw_download_str.tmp (
+	%curl% -s %lookup_base_url%%product_series_typeid% | %grep% -P --only-matching "(?s).*?(\r\n|\r|\n)" | %grep% -v "LookupValue"
+  )
+  type raw_download_str.tmp
+  exit /b %errorlevel%
+)
+
+>raw_download_str.tmp (
+  %curl% -s %lookup_base_url%%os_typeid% ^
+  | %grep% -P --only-matching "(?s)<Name>Windows 10 64-bit</Name>.*?</Value>" | %grep% -P --only-matching "(?<=<Value>).+(?=</Value>)" ^
+  | FIND "" /V
+)
+IF %ERRORLEVEL% NEQ 0 ( 
+  echo Cannot find os_id
+  exit /b %errorlevel% 
+) 
+set /p os_id=< raw_download_str.tmp
+echo "%os_name%" os_id: "%os_id%"
+
+>raw_download_str.tmp (
+  %curl% -s %lookup_base_url%%product_series_typeid% ^
+  | %grep% -P --only-matching "(?s)<Name>%product_series%</Name>.*?</Value>" | %grep% -P --only-matching "(?<=<Value>).+(?=</Value>)" ^
+  | FIND "" /V
+)
+IF %ERRORLEVEL% NEQ 0 ( 
+  echo Cannot find ps_id
+  exit /b %errorlevel% 
+) 
+set /p ps_id=< raw_download_str.tmp
+echo "%product_series%" ps_id: "%ps_id%"
+
+set search_query="%find_base_url%osid=%os_id%&psid=%ps_id%&dtcid=%dtc_id%"
+echo Get query: %search_query%
+
+>raw_download_str.tmp (
+  %curl% --location %search_query%
+)
+set /p result_url=< raw_download_str.tmp
+echo Get direct link from: "%result_url%"
+>result_url.tmp (
+  %curl% --location %result_url% | %grep% --only-matching "[^&]*whql.exe" | %grep% -P --only-matching "(?<=url=).*"
+)
+set /p downloadurl=< result_url.tmp
+echo Download driver: "%download_base_url%%downloadurl%"
+%curl% --location %download_base_url%%downloadurl% --remote-name
+
+FOR %%i IN ("%downloadurl%") DO (
+	set latest_filename=%%~ni%%~xi
+)
+
+echo Generating %latest_filename% autoinstall.bat
+>autoinstall.bat (
+  echo pushd "%%~dp0"
+  echo "%latest_filename%" -s
+  echo exit /b %%errorlevel%%
+)
+echo Done.
+
+exit /b %errorlevel%
