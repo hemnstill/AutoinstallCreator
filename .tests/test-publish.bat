a05aa9ebb1f1bf47f42e2f8e7ca50c0c44347a7a
Reshetnikov Alexandr <hemn.still@gmail.com>
2020-09-15 22:52:39 +0400

    Переезд в google drive

--- .tests/test-publish.bat
+++ .tests/test-publish.bat
@@ -34,35 +34,36 @@ if "%root_dir%"=="" (
   set root_dir=builds
 )
 
+set storage_provider=google-drive
 set rclone_config_name=rclone.conf.tmp
 >%rclone_config_name% (
-  echo [yandex-disk]
-  echo type = yandex
+  echo [%storage_provider%]
+  echo type = drive
   echo token = {"access_token":"%access_token%", "refresh_token":"%refresh_token%"}
 )
 
 echo publish to %version%_%branch% ...
-%rclone% --verbose --stats-one-line purge yandex-disk:%root_dir%/%version%_%branch%/ --config %rclone_config_name%
-%rclone% --verbose --stats-one-line --exclude .*/ --exclude *.tmp --exclude create_install.* copy ../ yandex-disk:%root_dir%/%version%_%branch%/ --config %rclone_config_name%
+%rclone% --verbose --stats-one-line purge %storage_provider%:%root_dir%/%version%_%branch%/ --config %rclone_config_name%
+%rclone% --verbose --stats-one-line --exclude .*/ --exclude *.tmp --exclude create_install.* copy ../ %storage_provider%:%root_dir%/%version%_%branch%/ --config %rclone_config_name%
 IF %ERRORLEVEL% NEQ 0 ( exit /b %ERRORLEVEL% ) 
 
 echo copy %version%_%branch% to latest_%branch% ...
-%rclone% --verbose --stats-one-line delete --rmdirs yandex-disk:%root_dir%/latest_%branch%/ --config %rclone_config_name%
-%rclone% --verbose --stats-one-line copy yandex-disk:%root_dir%/%version%_%branch%/ yandex-disk:%root_dir%/latest_%branch%/ --config %rclone_config_name%
+%rclone% --verbose --stats-one-line delete --rmdirs %storage_provider%:%root_dir%/latest_%branch%/ --config %rclone_config_name%
+%rclone% --verbose --stats-one-line copy %storage_provider%:%root_dir%/%version%_%branch%/ %storage_provider%:%root_dir%/latest_%branch%/ --config %rclone_config_name%
 IF %ERRORLEVEL% NEQ 0 ( exit /b %ERRORLEVEL% ) 
 
 set obsolete_dirs=obsolete_dirs.tmp
 >%obsolete_dirs% (
-	%rclone% --verbose lsf yandex-disk:%root_dir%/ --config %rclone_config_name% | %grep% "1\.0\." | %sort% --version-sort --reverse | more +6
+	%rclone% --verbose lsf %storage_provider%:%root_dir%/ --config %rclone_config_name% | %grep% "1\.0\." | %sort% --version-sort --reverse | more +6
 )
 for /f "tokens=*" %%a in (%obsolete_dirs%) do (
   if not "%%a"=="" (
 	echo purge obsolete version: %%a
-	%rclone% --verbose --stats-one-line purge yandex-disk:%root_dir%/%%a --config %rclone_config_name%
+	%rclone% --verbose --stats-one-line purge %storage_provider%:%root_dir%/%%a --config %rclone_config_name%
   )
 )
 
 echo cleanup
-%rclone% cleanup yandex-disk: --config %rclone_config_name%
+%rclone% cleanup %storage_provider%: --config %rclone_config_name%
 
 exit /b %errorlevel%
