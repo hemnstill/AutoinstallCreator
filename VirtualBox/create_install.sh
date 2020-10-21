c736f0115913552173cca82414c473d105292651
Reshetnikov Alexandr <hemn.still@gmail.com>
2020-10-21 09:01:50 +0400

    Добавил проверку на наличие MINGW64 перед созданием autoinstall.bat

--- VirtualBox/create_install.sh
+++ VirtualBox/create_install.sh
@@ -14,11 +14,15 @@ echo "Downloading: $download_url ..."
 $curl --location "$download_url" --output $vbox_file_name
 errorlevel=$?; if [[ $errorlevel -ne 0 ]]; then exit $errorlevel; fi
 
+[[ $(uname) != MINGW64* ]] && {
+  echo "Done. MINGW64 required for create autoinstall.bat"
+  exit
+}
 echo "Remove *.msi && extracting from: $vbox_file_name ..."
 rm -f *.msi && ./$vbox_file_name --silent --extract --path .
 errorlevel=$?; if [[ $errorlevel -ne 0 ]]; then exit $errorlevel; fi
 
-msi_file_name="%~dp0$(ls *.msi)"
+msi_file_name="%~dp0$(ls ./*.msi)"
 { printf '"%s" /passive /norestart
 exit /b %%errorlevel%%' "$msi_file_name"
 } > autoinstall.bat
