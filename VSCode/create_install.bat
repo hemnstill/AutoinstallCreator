1f1b54a1aba3d3b11d0e4c13e96e6f574d132731
Reshetnikov Alexandr <hemn.still@gmail.com>
2021-01-19 11:20:21 +0400

    Добавил автоустановку VSCode в режиме User

---
+++ VSCode/create_install.bat
@@ -0,0 +1,24 @@
+@echo off
+pushd "%~dp0"
+
+set curl=..\curl --fail
+set p7z=..\7z.exe
+set LC_ALL=en_US.UTF-8
+set grep=..\grep
+
+
+set latest_filename=VSCodeUserSetup.exe
+set download_url="https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user"
+echo Downloading: %download_url% ...
+rem %curl% --location %download_url% --output "%latest_filename%"
+if %errorlevel% neq 0 (
+  echo Cannot download latest version
+  exit /b %errorlevel%
+)
+
+echo Generating autoinstall.bat
+> autoinstall.bat (
+  echo "%%~dp0%latest_filename%" /SILENT /NORESTART"
+)
+
+echo Done.
