34378acd0482ed3b7723a9211c1892931891db49
Reshetnikov Alexandr <hemn.still@gmail.com>
2021-03-16 02:37:06 +0400

    Изабвился от Git-Credential-Manager-for-Windows (он входит в состав git-for-windows)

--- Git/create_install.bat
+++ Git/create_install.bat
@@ -24,30 +24,7 @@ for %%i in ("%download_url%") do (
     set git_latest_filename=%%~ni%%~xi
 )
 
-set latest_version=https://api.github.com/repos/microsoft/Git-Credential-Manager-for-Windows/releases/latest
-echo Get latest version: %latest_version% ...
->raw_download_str.tmp (
-	%curl% %latest_version% | findstr /r /c:"browser_download_url.*.exe\""
-)
-if %errorlevel% neq 0 (
-	echo Cannot get latest version
-	exit /b %errorlevel%
-)
-
-set /p download_url= < raw_download_str.tmp
-call set download_url=%%download_url:"browser_download_url":=%%
-call set download_url=%%download_url:"=%%
-echo Downloading: %download_url% ...
-%curl% --remote-name --location %download_url%
-if %errorlevel% neq 0 ( exit /b %errorlevel% )
-echo Done.
-
-for %%i in ("%download_url%") do (
-    set gcmw_latest_filename=%%~ni%%~xi
-)
-
 echo Generating autoinstall.bat
 > autoinstall.bat (
   echo "%%~dp0%git_latest_filename%" /SILENT /NORESTART /CLOSEAPPLICATIONS /LOADINF="%%~dp0git.ini"
-  echo "%%~dp0%gcmw_latest_filename%" /SILENT /NORESTART /CLOSEAPPLICATIONS
 )
