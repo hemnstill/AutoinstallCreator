e1a4f0d50c618a5fb609b38319417ecb6dd1621e
Reshetnikov Alexandr <hemn.still@gmail.com>
2021-03-20 16:38:42 +0400

    Немного портабилизировал _7z. Теперь не требуется 7z.exe и cp.exe для обновления 7z.exe
    Работает через bsdtar + busybox + 7za

--- _7z/create_install.bat
+++ _7z/create_install.bat
@@ -3,7 +3,14 @@ pushd "%~dp0"
 set curl=..\curl --fail
 set grep=..\grep
 set p7z=..\7z.exe
-set cp=..\cp.exe
+set p7za=7za.exe
+
+set bsdtar=..\_bsdtar\bsdtar.exe
+if not exist %bsdtar% (
+	call ..\.tests\test-run.bat _bsdtar create
+	if %errorlevel% neq 0 ( exit /b %errorlevel% )
+	pushd "%~dp0"
+)
 
 set latest_version=https://www.7-zip.org/download.html
 >raw_download_str.tmp (
@@ -22,12 +29,35 @@ if %errorlevel% neq 0 ( exit /b %errorlevel% )
 echo Done.
 
 for %%i in ("%download_url%") do (
+	set p7z_sfx_latest_filename=%%~ni%%~xi
+)
+
+set latest_version=https://www.7-zip.org/download.html
+>raw_download_str.tmp (
+  %curl% %latest_version% | %grep% --only-matching "[^ ]*extra.7z"
+)
+if %errorlevel% neq 0 (
+  echo Cannot get latest version
+  exit /b %errorlevel%
+)
+
+set /p download_url=< raw_download_str.tmp
+set download_url=https://www.7-zip.org/%download_url:~6%
+echo Downloading: %download_url% ...
+%curl% --remote-name --location %download_url%
+if %errorlevel% neq 0 ( exit /b %errorlevel% )
+echo Done.
+
+for %%i in ("%download_url%") do (
 	set latest_filename=%%~ni%%~xi
 )
+
+"%bsdtar%" --strip-components=1 -xf %latest_filename% x64
+if %errorlevel% neq 0 ( exit /b %errorlevel% )
+
 echo Generating %latest_filename% autoinstall.bat
 >autoinstall.bat (
   echo pushd "%%~dp0"
-  echo "%p7z%" e "%latest_filename%" "-o." 7z.exe 7z.dll -aoa -r
-  echo "%cp%" -v 7z.* ../
+  echo "%p7za%" e "%p7z_sfx_latest_filename%" "-o.." 7z.exe 7z.dll -aoa -r
   echo exit /b %%errorlevel%%
 )
