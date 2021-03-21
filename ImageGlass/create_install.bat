6b04de3d6de295f45ba001ba10d5ac1797eddcb9
Reshetnikov Alexandr <hemn.still@gmail.com>
2021-03-22 00:52:50 +0400

    github_releases.py d2phap ImageGlass

---
+++ ImageGlass/create_install.bat
@@ -0,0 +1,36 @@
+@echo off
+pushd "%~dp0"
+
+set curl=..\curl --fail
+set p7z=..\7z.exe
+set grep=..\grep
+
+set latest_version=https://api.github.com/repos/d2phap/ImageGlass/releases/latest
+echo Get latest version: %latest_version% ...
+>raw_download_str.tmp (
+    %curl% %latest_version% | %grep% """browser_download_url""" | %grep% --only-matching "[^"" ]*x64\.msi"
+)
+if %errorlevel% neq 0 (
+  echo Cannot get latest version
+  exit /b %errorlevel%
+)
+
+set /p download_url= < raw_download_str.tmp
+echo Downloading: %download_url% ...
+%curl% --remote-name --location %download_url%
+if %errorlevel% neq 0 (
+  echo Cannot download latest version
+  exit /b %errorlevel%
+)
+
+for %%i in ("%download_url%") do (
+  set latest_filename=%%~ni%%~xi
+)
+
+echo Generating %latest_filename% autoinstall.bat
+> autoinstall.bat (
+	echo "%%~dp0%latest_filename%" /passive
+	echo exit /b %%errorlevel%%
+)
+
+echo Done.
