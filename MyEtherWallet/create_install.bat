ebf959666da29ecc16fb6b485e5a5ef147a1404c
Reshetnikov Alexandr <hemn.still@gmail.com>
2021-05-13 08:24:15 +0400

    Скачивание и распаковка MyEtherWallet

---
+++ MyEtherWallet/create_install.bat
@@ -0,0 +1,37 @@
+@echo off
+pushd "%~dp0"
+
+set curl=..\curl --fail
+set p7z=..\7z.exe
+set grep=..\grep
+
+set latest_version=https://api.github.com/repos/MyEtherWallet/MyEtherWallet/releases/latest
+echo Get latest version: %latest_version% ...
+>raw_download_str.tmp (
+    %curl% %latest_version% | %grep% """browser_download_url""" | %grep% --only-matching "[^"" ]*MyEtherWallet-[^"" ]*\.zip"
+)
+if %errorlevel% neq 0 (
+  echo Cannot get latest version
+  exit /b %errorlevel%
+)
+
+set /p download_url= < raw_download_str.tmp
+echo Downloading: %download_url% ...
+%curl% --remote-name --location %download_url%
+if %errorlevel% neq 0 (
+  echo Cannot download latest version
+  exit /b %errorlevel%
+)
+
+for %%i in (%download_url%) do (
+  set latest_filename=%%~ni%%~xi
+  set latest_dirname=%%~ni
+)
+
+echo Generating %latest_filename% autoinstall.bat
+> autoinstall.bat (
+	echo "%%~dp0%p7z%" x "%%~dp0%latest_filename%" "-o%%~dp0%latest_dirname%" -aoa -r
+	echo exit /b %%errorlevel%%
+)
+
+echo Done.
