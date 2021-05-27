b933a9a5ee828e46e134abfc973af5a533d345ac
Reshetnikov Alexandr <hemn.still@gmail.com>
2021-05-27 10:36:45 +0400

    Скачиваем и устанавливаем https://www.isunshare.com/clonego/
    Для работы в обратную сторону: vhdx2disk

---
+++ CloneGo/create_install.bat
@@ -0,0 +1,34 @@
+@echo off
+pushd "%~dp0"
+
+set curl=..\curl --fail
+set p7z=..\7z.exe
+set LC_ALL=en_US.UTF-8
+set grep=..\grep
+
+set latest_version="https://www.isunshare.com/clonego"
+echo Downloading: %latest_version% ...
+>raw_download_str.tmp (
+    %curl% --location %latest_version% | %grep% -Po "(?<=href="")[^\s]*\.exe(?="")"
+)
+if %errorlevel% neq 0 (
+  echo Cannot download latest version
+  exit /b %errorlevel%
+)
+
+set /p download_url= < raw_download_str.tmp
+echo Downloading: %download_url% ...
+%curl% --remote-name --location %download_url%
+if %errorlevel% neq 0 (
+  echo Cannot download latest version
+  exit /b %errorlevel%
+)
+
+for %%i in (%download_url%) do (
+  set latest_filename=%%~ni%%~xi
+)
+
+echo Generating %latest_filename% autoinstall.bat
+echo "%%~dp0%latest_filename%" /S > autoinstall.bat
+
+echo Done.
