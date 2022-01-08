06352afb8e0a4071db9c477d6880cadcac512454
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-01-08 22:42:11 +0400

    ~~

--- 7-zip/create_install.bat
+++ 7-zip/create_install.bat
@@ -1,7 +1,5 @@
-@echo off
-pushd "%~dp0"
-set curl=..\curl --fail
-set grep=..\grep
+@pushd "%~dp0"
+@call ../.src/env_tools.bat
 
 set latest_version=https://www.7-zip.org/download.html
 >raw_download_str.tmp (
--- 7tt/create_install.bat
+++ 7tt/create_install.bat
@@ -1,6 +1,5 @@
-@echo off
-pushd "%~dp0"
-set curl=..\curl --fail
+@pushd "%~dp0"
+@call ../.src/env_tools.bat
 
 set download_url="https://rammichael.com/downloads/7tt_setup.exe"
 echo Downloading: %download_url% ...
