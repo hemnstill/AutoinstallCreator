e40e5be7f246072a9ccb73359ca5abd2a1e37bc6
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2022-01-11 18:38:05 +0400

    curl 7.81: linux static build from moparisthebest/static-curl (#2)

--- README.md
+++ README.md
@@ -15,5 +15,5 @@ run `<Application>/autoinstall.bat`
 
 ## Tools used:
 * 7z 21.7.0.0 https://www.7-zip.org/
-* curl 7.71.1.0 https://curl.haxx.se/windows/
+* curl 7.81.0.0 https://curl.haxx.se/windows/
 * grep 3.1 https://github.com/git-for-windows/git-sdk-64/tree/main/usr/bin
--- _curl/create_install.bat
+++ _curl/create_install.bat
@@ -1,9 +1,24 @@
 @pushd "%~dp0"
 @call ..\.src\env_tools.bat
 
+set busybox=..\_busybox\busybox64.exe
+if not exist %busybox% (
+	call ..\.tests\test-run.bat _busybox create
+	if %errorlevel% neq 0 ( exit /b %errorlevel% )
+	pushd "%~dp0"
+)
+
+set for_linux=%1
+
+set search_pattern=--only-matching "dl[^ ]*win64-mingw\.zip" ^| find "" /V
 set latest_version=https://curl.se/windows/
+if not "%for_linux%" == "" (
+  set search_pattern="""browser_download_url""" ^| %grep% --only-matching "[^"" ]*curl-amd64"
+  set latest_version=https://api.github.com/repos/moparisthebest/static-curl/releases/latest
+)
+
 >raw_download_str.tmp (
-  %curl% %latest_version% | %grep% --only-matching "dl[^ ]*win64-mingw\.zip" | find "" /V
+  %curl% %latest_version% | %grep% %search_pattern%
 )
 if %errorlevel% neq 0 (
   echo Cannot get latest version
@@ -11,7 +26,10 @@ if %errorlevel% neq 0 (
 )
 
 set /p download_url=< raw_download_str.tmp
-set download_url=%latest_version%%download_url%
+if "%for_linux%" == "" (
+  set download_url=%latest_version%%download_url%
+)
+
 echo Downloading: %download_url% ...
 %curl% --remote-name --location %download_url%
 if %errorlevel% neq 0 ( exit /b %errorlevel% )
@@ -21,10 +39,15 @@ for %%i in ("%download_url%") do (
   set latest_filename=%%~ni%%~xi
 )
 
+set extract_command="%p7z%" e "%latest_filename%" "-o.." *.exe *.crt -aoa -r
+if not "%for_linux%" == "" (
+  set extract_command=%busybox% cp -v %latest_filename% ..
+)
+
 echo Generating %latest_filename% autoinstall.bat
 >autoinstall.bat (
   echo pushd "%%~dp0"
-  echo "%p7z%" e "%latest_filename%" "-o.." *.exe *.crt -aoa -r
+  echo %extract_command%
   echo exit /b %%errorlevel%%
 )
 echo Done.
