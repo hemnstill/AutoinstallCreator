76f66d43588fb0e66c6b3263ebbad10b358fe01b
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-02-01 00:12:37 +0400

    ~ -bash

--- .tests/test-run.mingw64.bat
+++ .tests/test-run.mingw64.bat
@@ -1,11 +1,5 @@
 @echo off
-set test_run_bat="%~dp0..\.tests\test-run.bat"
 set test_run_sh="%~dp0..\.tests\test-run.sh"
-
 set bash="%~dp0..\.tools\busybox64.exe" bash
-if not exist %bash% (
-  call %test_run_bat% _bash create
-  if %errorlevel% neq 0 ( exit /b %errorlevel% )
-)
 
 %bash% %test_run_sh% %*
