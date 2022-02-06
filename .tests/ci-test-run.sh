4628f151fc475f3ef935c795fa3dfc11a87647cf
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-02-06 20:58:13 +0400

    || exit

--- .tests/ci-test-run.sh
+++ .tests/ci-test-run.sh
@@ -3,6 +3,6 @@ dp0="$(dirname "$0")"
 export CI=true
 dp0_tools="$dp0/../.tools" && source "$dp0_tools/env_tools.sh"
 
-"$dp0/test-tools.sh"
+"$dp0/test-tools.sh" || exit
 
-"$dp0/test-run.sh" _ create
+"$dp0/test-run.sh" _ create || exit 
