1fcd2032333cdb663a9085e58994a3541d58ae4c
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-02-06 21:05:21 +0400

    linux env_tools

--- .tests/test-tools.sh
+++ .tests/test-tools.sh
@@ -26,14 +26,24 @@ echo ">> env:"
 uname -a
 echo is_windows: "$is_windows_os"
 
-test_stdout "$curl --version" "curl 7.81.0 (x86_64-pc-win32) libcurl/7.81.0 OpenSSL/3.0.1 (Schannel) zlib/1.2.11 brotli/1.0.9 libidn2/2.3.2 libssh2/1.10.0 nghttp2/1.46.0 libgsasl/1.10.0
-Release-Date: 2022-01-05
-Protocols: dict file ftp ftps gopher gophers http https imap imaps ldap ldaps mqtt pop3 pop3s rtsp scp sftp smb smbs smtp smtps telnet tftp \nFeatures: alt-svc AsynchDNS brotli gsasl HSTS HTTP2 HTTPS-proxy IDN IPv6 Kerberos Largefile libz MultiSSL NTLM SPNEGO SSL SSPI TLS-SRP UnixSockets"
+if [[ $is_windows_os ]]; then
+  test_stdout "$curl --version" "curl 7.81.0 (x86_64-pc-win32) libcurl/7.81.0 OpenSSL/3.0.1 (Schannel) zlib/1.2.11 brotli/1.0.9 libidn2/2.3.2 libssh2/1.10.0 nghttp2/1.46.0 libgsasl/1.10.0
+  Release-Date: 2022-01-05
+  Protocols: dict file ftp ftps gopher gophers http https imap imaps ldap ldaps mqtt pop3 pop3s rtsp scp sftp smb smbs smtp smtps telnet tftp \nFeatures: alt-svc AsynchDNS brotli gsasl HSTS HTTP2 HTTPS-proxy IDN IPv6 Kerberos Largefile libz MultiSSL NTLM SPNEGO SSL SSPI TLS-SRP UnixSockets"
 
-test_stdout "$grep --version" "pcre2grep version 10.39 2021-10-29"
-test_stdout "$p7zip --" "
-7-Zip 21.07 (x64) : Copyright (c) 1999-2021 Igor Pavlov : 2021-12-26"
-test_stdout "$busybox tar --version" "tar (busybox) 1.35.0-FRP-4487-gd239d2d52"
+  test_stdout "$grep --version" "pcre2grep version 10.39 2021-10-29"
+  test_stdout "$p7zip --" "
+  7-Zip 21.07 (x64) : Copyright (c) 1999-2021 Igor Pavlov : 2021-12-26"
+  test_stdout "$busybox tar --version" "tar (busybox) 1.35.0-FRP-4487-gd239d2d52"
+else
+  test_stdout "$curl --version" "curl 7.81.0 (x86_64-pc-linux-musl) libcurl/7.81.0 OpenSSL/3.0.1 (Schannel) zlib/1.2.11 brotli/1.0.9 libidn2/2.3.2 libssh2/1.10.0 nghttp2/1.46.0 libgsasl/1.10.0
+  Release-Date: 2022-01-05
+  Protocols: dict file ftp ftps gopher gophers http https imap imaps ldap ldaps mqtt pop3 pop3s rtsp scp sftp smb smbs smtp smtps telnet tftp \nFeatures: alt-svc AsynchDNS HSTS HTTP2 HTTPS-proxy IPv6 Largefile libz NTLM NTLM_WB SSL TLS-SRP UnixSockets"
+
+  test_stdout "$grep --version" "pcre2grep version 10.39 2021-10-29"
+  test_stdout "$p7zip --" "
+  7-Zip 21.07 (x64) : Copyright (c) 1999-2021 Igor Pavlov : 2021-12-26"
+fi
 
 echo Errors: $errors_count
 exit $errors_count
