1347bb84a575573ec050a32f3abb4c762b0c388f
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2022-02-13 19:45:45 +0400

    7tt .sh (#14)

--- 7tt/create_install.bat
+++ 7tt/create_install.bat
@@ -1,14 +1,4 @@
-@pushd "%~dp0"
-@call "%~dp0..\.tools\env_tools.bat"
-
-set download_url="https://rammichael.com/downloads/7tt_setup.exe"
-echo Downloading: %download_url% ...
-%curl% --remote-name --location %download_url%
-if %errorlevel% neq 0 ( exit /b %errorlevel% )
-echo Done.
-
-for %%i in (%download_url%) do (
-  set latest_filename=%%~ni%%~xi
-)
-echo Generating %latest_filename% autoinstall.bat
-echo "%%~dp0%latest_filename%" /S > "%~dp0autoinstall.bat"
+@echo off
+call "%~dp0..\.tools\env_tools.bat"
+%busybox% bash "%~dp0create_install.sh"
+exit /b %errorlevel%
