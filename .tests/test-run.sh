394f54bd90dc8b7a3b00e40e4f61789c4c6fcad2
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-05-29 22:52:32 +0400

    ↑

--- .tests/test-run.sh
+++ .tests/test-run.sh
@@ -4,7 +4,7 @@ dp0_tools="$dp0/../.tools" && source "$dp0_tools/env_tools.sh"
 set -e
 
 if [[ "$is_windows_os" == true ]]; then
-  "$dp0/test-run-sh.sh" -- testInternalCreate || exit 1
-else
   "$dp0/test-run-bat.sh" -- testInternalCreate || exit 1
+else
+  "$dp0/test-run-sh.sh" -- testInternalCreate || exit 1
 fi
