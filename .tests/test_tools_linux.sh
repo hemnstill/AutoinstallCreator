c45a898fdeb68cb321b7ba567c3e6aecdc034d45
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-05-29 14:41:04 +0400

    `

---
+++ .tests/test_tools_linux.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+dp0="$(realpath "$(dirname "$0")")"
+dp0_tools="$dp0/../.tools" && source "$dp0_tools/env_tools.sh"
+
+testCurlVersion() {
+  local etalon_log="$(echo -e "curl 7.81.0 (x86_64-pc-linux-musl) libcurl/7.81.0 OpenSSL/1.1.1l zlib/1.2.11 libssh2/1.9.0 nghttp2/1.43.0
+Release-Date: 2022-01-05
+Protocols: dict file ftp ftps gopher gophers http https imap imaps mqtt pop3 pop3s rtsp scp sftp smb smbs smtp smtps telnet tftp \nFeatures: alt-svc AsynchDNS HSTS HTTP2 HTTPS-proxy IPv6 Largefile libz NTLM NTLM_WB SSL TLS-SRP UnixSockets")"
+  assertEquals "$etalon_log" "$($curl --version)"
+}
+
+testGrepVersion() {
+  assertEquals "pcre2grep version 10.40 2022-04-14" "$("$grep" --version)"
+}
+
+test7zVersion() {
+  assertEquals "
+7-Zip 21.07 (x64) : Copyright (c) 1999-2021 Igor Pavlov : 2021-12-26" "$("$p7z" | head -2)"
+}
+
+testBusyboxVersion() {
+  assertEquals "tar (busybox) 1.34.1" "$("$busybox" tar --version)"
+}
+
+# Load and run shUnit2.
+source "../.tests/shunit2/shunit2"
