f018c79b327955f5aca90c69e91afc1b3e7ce7a8
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-05-29 09:25:08 +0400

    download electrum

---
+++ Electrum/create_install.bat
@@ -0,0 +1,3 @@
+@echo off
+"%~dp0..\.tools\busybox64.exe" bash "%~dp0create_install.sh"
+exit /b %errorlevel%
---
+++ Electrum/create_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+dp0="$(realpath "$(dirname "$0")")"
+dp0_tools="$dp0/../.tools" && source "$dp0_tools/env_tools.sh"
+cd "$dp0" || exit 1
+
+html_url='https://electrum.org/panel-download.html'
+echo Get latest version: $html_url ...
+download_url=$($curl --silent --location "$html_url" | "$grep" --only-matching '(?<=href=")[^,]+-setup\.exe(?=")' | head -1)
+[[ -z "$download_url" ]] && {
+  echo "Cannot get release version"
+  exit 1
+}
+
+echo "Downloading: $download_url ..."
+$curl --location "$download_url" --remote-name
+errorlevel=$?
+if [[ $errorlevel -ne 0 ]]; then exit $errorlevel; fi
+
+
+
