c8e79a4fb0069d52175e0df2608582dad6e6402f
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2022-05-29 14:03:50 +0400

    Bsdtar zstd (#21)
    add shunit2 47be8b23a46a7897e849f1841f0fb704d34d0f6e
    add test_windows.sh on windows

--- _bsdtar/create_install.sh
+++ _bsdtar/create_install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 dp0="$(realpath "$(dirname "$0")")"
 dp0_tools="$dp0/../.tools" && source "$dp0_tools/env_tools.sh"
-cd "$dp0" || exit 1
+set -e
+cd "$dp0"
 
-api_url='https://api.github.com/repos/libarchive/libarchive/releases/latest'
-echo Get latest version: $api_url ...
-download_url=$($curl --silent --location "$api_url" | "$grep" --only-matching '(?<="browser_download_url":\s")[^,]+(amd64|win64)\.zip(?=")' | head -1)
+api_url='https://api.github.com/repos/hemnstill/StandaloneTools/releases?per_page=100'
+echo Get latest version: "$api_url" ...
+download_url=$($curl --silent --location "$api_url" | "$grep" --only-matching '(?<="browser_download_url":\s")[^,]+bsdtar-[^,]+build-mingw\.tar\.gz(?=")' | head -1)
 [[ -z "$download_url" ]] && {
   echo "Cannot get release version"
   exit 1
@@ -13,12 +14,11 @@ download_url=$($curl --silent --location "$api_url" | "$grep" --only-matching '(
 
 echo "Downloading: $download_url ..."
 $curl --location "$download_url" --remote-name
-errorlevel=$?
-if [[ $errorlevel -ne 0 ]]; then exit $errorlevel; fi
 
 zip_file_name="$(basename -- "$download_url")"
-"$p7z" e "$zip_file_name" "-o." bsdtar.exe -aoa -r
-check_command="ls" && $is_windows_os && check_command="$dp0/bsdtar.exe --version"
-echo check:
-$check_command
+"$p7z" e "$zip_file_name" -so | "$p7z" e -si -ttar "-o." bsdtar.exe -aoa -r
+
+if [[ "$is_windows_os" == true ]]; then
+  ./test_windows.sh;
+fi
 
