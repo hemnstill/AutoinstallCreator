3d873fbda41629e30360a43ad102a48869362a06
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-05-29 22:20:22 +0400

    testExternalCreate

---
+++ .tests/test-run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+dp0="$(realpath "$(dirname "$0")")"
+dp0_tools="$dp0/../.tools" && source "$dp0_tools/env_tools.sh"
+set -e
+cd "$dp0"
+
+testInternalCreate() {
+  "$busybox" bash "$dp0_tools/batch_runner.sh" _ create || fail
+}
+
+testExternalCreate() {
+  "$busybox" bash "$dp0_tools/batch_runner.sh" "" create || fail
+}
+
+# Load and run shUnit2.
+source "./shunit2/shunit2"
