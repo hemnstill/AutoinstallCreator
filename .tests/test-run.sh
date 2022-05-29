3069d0945ed71a71e5516f891d99f8ebf8417676
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-05-29 22:57:57 +0400

    "$@"

--- .tests/test-run.sh
+++ .tests/test-run.sh
@@ -4,7 +4,7 @@ dp0_tools="$dp0/../.tools" && source "$dp0_tools/env_tools.sh"
 set -e
 
 if [[ "$is_windows_os" == true ]]; then
-  "$dp0/test-run-bat.sh" || exit 1
+  "$dp0/test-run-bat.sh" "$@" || exit 1
 else
-  "$dp0/test-run-sh.sh" || exit 1
+  "$dp0/test-run-sh.sh" "$@" || exit 1
 fi
