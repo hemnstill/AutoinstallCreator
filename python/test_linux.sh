ea6e09553b2f39860eb4e02a2d69afc7aa26b6d0
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-11-19 00:29:45 +0400

    linux tests

--- python/test_linux.sh
+++ python/test_linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 testVersion() {
-  version_info="$("./Scripts/python.exe" --version | head -c 9)"
+  version_info="$("./Scripts/bin/python3" --version | head -c 9)"
   assertEquals "$version_info" "Python 3."
 }
 
