71d205ee5542629cf7d90c47c91e92ff2ef3ea49
Reshetnikov Alexandr <hemn.still@gmail.com>
2022-11-25 23:39:32 +0400

    rename

--- ReSharper.CommandLineTools/create_install.sh
+++ ReSharper.CommandLineTools/create_install.sh
@@ -7,8 +7,8 @@ cd "$dp0"
 api_url='https://data.services.jetbrains.com/products/releases?code=RS%2CRSU%2CRSCLT%2CDCCLT%2CDMCLP%2CDPCLT&latest=true&type=release'
 echo Get latest version: "$api_url" ...
 download_url=$($curl --location "$api_url" | "$grep" --only-matching "(?<=\"link\":\")[^,]+CommandLineTools[^,]+\.zip(?=\")" | head -n1)
-echo Downloading: $download_url ...
-$curl --location $download_url --remote-name
+echo Downloading: "$download_url" ...
+$curl --location "$download_url" --remote-name
 errorlevel=$?sync
 if [[ $errorlevel -ne 0 ]]; then exit $errorlevel; fi
 
