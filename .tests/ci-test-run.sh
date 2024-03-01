f3f2e8c2f88c94512d50de8672499437ddf8accb
Reshetnikov_AS <hemn.still@gmail.com>
2024-03-02 02:43:25 +0400

    -v

--- .tests/ci-test-run.sh
+++ .tests/ci-test-run.sh
@@ -8,6 +8,6 @@ set -e
 
 "$dp0/test-tools.sh" || exit 1
 
-cd "$dp0/../_AutoinstallCreator" && python3 -m unittest || exit 1
+cd "$dp0/../_AutoinstallCreator" && python3 -m unittest -v || exit 1
 
 "$dp0/test-run.sh" -- testInternalCreate || exit 1
