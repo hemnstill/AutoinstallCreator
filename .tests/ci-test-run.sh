50302e1c0fd58ed8a9c38e6b31800f03fbf5bb66
Reshetnikov_AS <hemn.still@gmail.com>
2024-03-01 23:39:38 +0400

    enable windows tests

--- .tests/ci-test-run.sh
+++ .tests/ci-test-run.sh
@@ -8,8 +8,6 @@ set -e
 
 "$dp0/test-tools.sh" || exit 1
 
-if [[ "$is_windows_os" == false ]]; then
-  cd "$dp0/../_AutoinstallCreator" && python3 -m unittest || exit 1
-fi
+cd "$dp0/../_AutoinstallCreator" && python3 -m unittest || exit 1
 
 "$dp0/test-run.sh" -- testInternalCreate || exit 1
